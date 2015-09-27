b025fe75232a03007dcf8d12fe1577354aa48a57
Austin Miller <mill1751@purdue.edu>
2015-09-27 12:29:53 -0400

    finished spawn system with size system

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float playerSize;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onCollisionEnter(Collider c) {
+		print ("success");
+	}
+
+	void onTriggerEnter(Collider c) {
+		print ("success2");
+	}
+
+}
--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -4,30 +4,45 @@ using System.Collections;
 public class SpawnSystem : MonoBehaviour {
 
 	public float minSpawnDistanceFromPlayer;
+	public int numOfEnemies;
+	public float enemySizeRangeFromPlayer;
 	public GameObject SuperCell;
 	public GameObject Spiro;
-
+	
 	int count;
+	float playerSize;
 
 	void Start () {
-		while (count < 15) {
+		playerSize = GameObject.Find ("PlayerCell").GetComponent<PlayerScript>().playerSize;
 
-			float i = Random.value * 20;
-			float j = Random.value * 20;
+		while (count < numOfEnemies) {
+			float i = Random.value * 20 * (Random.value > 0.5? 1 : -1);
+			float j = Random.value * 20 * (Random.value > 0.5? 1 : -1);
 			Vector3 position = new Vector3(i, 1, j);
 			while(Vector3.Distance(position, transform.position) < minSpawnDistanceFromPlayer
 			      && Vector3.Distance(position, GameObject.Find("PetriDish").transform.position) < 20) {
 				i = Random.value * 20;
 				j = Random.value * 20;
-				position = new Vector3(i, 1, j);
+				position = new Vector3(i, 0.77f, j);
 			}
 
-			switch(Random.Range(0, 1)) {
+			float enemySize = Random.Range(playerSize - enemySizeRangeFromPlayer, playerSize + enemySizeRangeFromPlayer);
+
+			switch(Random.Range(0, 2)) {
 			case 0:
-				Instantiate(SuperCell, position, new Quaternion(0, 0, 0, 0));
+				GameObject newSuperCell = (GameObject) Instantiate(SuperCell, position, new Quaternion(0, 0, 0, 0));
+				newSuperCell.GetComponent<SuperCellAI>().size = enemySize;
+				newSuperCell.transform.localScale *= enemySize;
+				count++;
 				break;
 			case 1:
-				Instantiate(Spiro, position, new Quaternion(0, 0, 0, 0));
+				GameObject newSpiro = (GameObject) Instantiate(Spiro, position, new Quaternion(0, 0, 0, 0));
+				newSpiro.GetComponent<SpiroAI>().size = enemySize;
+				newSpiro.transform.localScale *= enemySize;
+				count++;
+				break;
+
+			default:
 				break;
 			}
 		}
